ce5fd9b01fb4ded50a9481d804fe0615df5377a8
Kaycee Coleman <kaycee.coleman@gmail.com>
2017-12-03 21:01:29 -0500

    Update neamap.R

--- neamap.R
+++ neamap.R
@@ -40,25 +40,40 @@ neamap.wq = neamap.wq %>%
   group_by(STATION) %>% 
   summarise(CTD_SAMPLE_TIME = first(Time),
             CTD_SAMPLE_DEPTH = first(SAMPLE_DEPTH), 
-            DO = ifelse(length(VALUE[PARAMETER %in% 'DO'])>0,VALUE[PARAMETER %in% 'DO'],NA),
-            PS = ifelse(length(VALUE[PARAMETER %in% 'PS']>0),VALUE[PARAMETER %in% 'PS'],NA),
-            SA = ifelse(length(VALUE[PARAMETER %in% 'SA']>0),VALUE[PARAMETER %in% 'SA'],NA),
-            WT = VALUE[PARAMETER %in% 'WT']) %>% as.data.frame()
+            dobot = ifelse(length(VALUE[PARAMETER %in% 'DO'])>0,VALUE[PARAMETER %in% 'DO'],NA),
+            salbot = ifelse(length(VALUE[PARAMETER %in% 'SA']>0),VALUE[PARAMETER %in% 'SA'],NA),
+            tempbot = VALUE[PARAMETER %in% 'WT']) %>% as.data.frame()
   
-neamap.data = left_join(neamap.catch, dplyr::select(neamap.station, STATION,DATE,TowBegin,TowEnd,DepthBeg,
+neamap.data = left_join(neamap.catch, dplyr::select(neamap.station, STATION,DATE, TowBegin, REGION, DepthBeg,
                                                     DepthEnd,DepthStratum,DURATION,VSPEED,VSpeedGPS,
                                                     TOWDIST,TowDistTrack,LATITUDE_START,LONGITUDE_START,
                                                     LATITUDE_END,LONGITUDE_END), by = "STATION") %>%
   left_join(.,neamap.wq, by = "STATION") %>% rowwise %>%
   mutate(date = as.Date(sapply(strsplit(as.character(TowBegin)," "), head, 1),format="%Y-%m-%d"),
          year = as.numeric(format(date, format = "%Y")),
-         month = as.numeric(format(date, format = "%m"))) %>% 
-  transmute(lon = mean(LONGITUDE_START, LONGITUDE_END),
-         lat = mean(LATITUDE_START, LATITUDE_END),
-         depth = mean(DepthBeg, DepthEnd)) %>% as.data.frame()
+         month = as.numeric(format(date, format = "%m")),
+         lon = ifelse(!is.na(c(LONGITUDE_START & LONGITUDE_END)), mean(LONGITUDE_START, LONGITUDE_END), 
+                      c(LONGITUDE_START, LONGITUDE_END)[!is.na(c(LONGITUDE_START, LONGITUDE_END))]),
+         lat = ifelse(!is.na(c(LATITUDE_START & LATITUDE_END)), mean(LATITUDE_START, LATITUDE_END), 
+                      c(LATITUDE_START, LATITUDE_END)[!is.na(c(LATITUDE_START, LATITUDE_END))]),
+         depth = ifelse(!is.na(c(DepthBeg & DepthEnd)), mean(DepthBeg, DepthEnd), 
+                        c(DepthBeg, DepthEnd)[!is.na(c(DepthBeg, DepthEnd))]),
+         survey = 'neamap', stratum = as.numeric(REGION),
+         depth = depth*0.3048) %>% #change feet to meters
+  as.data.frame() %>% 
+  dplyr::select(-LONGITUDE_START, -LONGITUDE_END, -LATITUDE_START, -LATITUDE_END, -DepthBeg, -DepthEnd, 
+                -TowBegin, -VIMSCODE, -SpeciesRemarks,-date,-REGION) %>% 
+  filter(lon<c(-73.75) & lat<40.5) # trim at northern NJDEP data border
+names(neamap.data)=tolower(names(neamap.data))
 
-usa <- map_data("usa")
-ggplot(neamap.data,aes(lon,lat))+geom_point()+
-  geom_polygon(data = usa, aes(x=long, y = lat, group=group), fill="forestgreen")+
-  coord_fixed(xlim = c(-76,-70.5), ylim = c(37.5,42.2), ratio = 1.3)+theme_bw()
+# usa <- map_data("usa")
+# ggplot(neamap.data,aes(lon,lat))+geom_point()+
+#   geom_polygon(data = usa, aes(x=long, y = lat, group=group), fill="forestgreen")+
+#   coord_fixed(xlim = c(-76,-70.5), ylim = c(37.5,42.2), ratio = 1.3)+theme_bw()
+
+# make sure the regions are sampled consistently
+# ggplot(neamap.station[neamap.station$LATITUDE_START<40.5,],aes(REGION))+geom_bar()
+# ggplot(neamap.station[neamap.station$LATITUDE_START<40.5,],aes(LONGITUDE_START,LATITUDE_START,col=REGION))+geom_point()
+
+rm(neamap.catch,neamap.station,neamap.wq)
 # ------------------------ #
